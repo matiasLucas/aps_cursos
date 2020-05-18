471a686da4a659a941f1cb78b4cb1f4e5bb2e791
matiasLucas <teixeiramatiaslucas@gmail.com>
2020-05-18 15:40:25 -0300

    Listando csv

--- model/LeiaCsv.java
+++ model/LeiaCsv.java
@@ -1,5 +1,5 @@
 package model;
-
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,22 +8,29 @@ public class LeiaCsv {
 	public static void main(String[] args) {
 		LeiaCsv objLerCsv = new LeiaCsv();
 		String csvPath = "C:\\Users\\teixe\\OneDrive\\Documentos\\01\\alunos.csv";
-		objLerCsv.leitura(csvPath);
+		ArrayList<?> ret = objLerCsv.leitura(csvPath);
+	   // System.out.println("Aluno [id= " + ret[ret.length-2] 
+         //                  + " , nome=" + ret[ret.length-1] + "]");
+		System.out.println(ret);
 	}
 	
-	public void leitura(String arquivoCSV) {
+	public ArrayList<ArrayList<String>> leitura(String arquivoCSV) {
 		BufferedReader br = null;
 	    String linha = "";
-	    
-		    try {
+	    String[] aluno = null;
+	    ArrayList<String> l1 = new ArrayList<String>();
+	    ArrayList<String> l2 = new ArrayList<String>();
+	    ArrayList<ArrayList<String>> ret = new ArrayList<ArrayList<String>>();
+
+	    try {
 		        br = new BufferedReader(new FileReader(arquivoCSV));
 		        while ((linha = br.readLine()) != null) {
-
-		            String[] aluno = linha.split(";");
-
-		            System.out.println("Aluno [id= " + aluno[aluno.length-2] 
-		                                 + " , nome=" + aluno[aluno.length-1] + "]");
-		        }
+		            aluno = linha.split(";");
+		            l1.add(aluno[aluno.length-2]);
+		            l2.add(aluno[aluno.length-1]);          
+		          }
+		        ret.add(l1);
+	            ret.add(l2);  
 		    }catch (Exception e) {
 		    	e.printStackTrace();
 		        
@@ -34,7 +41,8 @@ public class LeiaCsv {
 		            	} catch (IOException e) {
 		            		e.printStackTrace();
 		            }
-		        }
-		    }		
+		        }	 
+		    }
+		    return ret; 
 	}
 }
